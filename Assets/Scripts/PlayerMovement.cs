877c07680011df67f19cd5ece046dc6b1d944708
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2025-02-15 17:34:27 +0000

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,6 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSmoothing;
-    private Vector2 movementVector;
     private Vector3 moveDirection;
     private float xMoveAmount;
     private float zMoveAmount;
@@ -38,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //TestInputAction();
         HandleMovement();
+
     }
 
     private void TestInputAction()
@@ -51,21 +51,37 @@ public class PlayerMovement : MonoBehaviour
     private void HandleMovement()
     {
         // read input
-        movementVector = movementInput.ReadValue<Vector2>();
+        Vector2 _movementVector = movementInput.ReadValue<Vector2>();
 
-        // comput moveDirection
-        moveDirection = new Vector3(movementVector.x, 0, movementVector.y);
+        // compute moveDirection
+        xMoveAmount = _movementVector.x;
+        zMoveAmount = _movementVector.y;
+        moveDirection = new Vector3(xMoveAmount, 0, zMoveAmount);
 
         // execute move
         if (movementInput.IsPressed())
         {
+            // apply walking logic
+            float _walkMultiplier = Mathf.Clamp01(Mathf.Abs(xMoveAmount) + Mathf.Abs(zMoveAmount));
+            if (_walkMultiplier > 0 && _walkMultiplier <= 0.9f)
+            {
+                _walkMultiplier = 0.5f;
+            }
+            else if (_walkMultiplier > 0.9f && _walkMultiplier <= 1)
+            {
+                _walkMultiplier = 1;
+            }
+
             // rotate
             Quaternion _targetRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, _targetRotation, rotationSmoothing * Time.fixedDeltaTime);
 
             // move forward
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.Self);
+            transform.Translate(Vector3.forward * (moveSpeed * _walkMultiplier) * Time.fixedDeltaTime, Space.Self);
+
         }
+
+
     }
 
 }
