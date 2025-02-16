05880e042720d0a01288ca95b2f956f58e6ff79d
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2025-02-16 12:15:13 +0000

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,14 @@ public class PlayerMovement : MonoBehaviour
 
     // cache
     private Rigidbody rb;
+    private GameObject playerChild;
+    private Animator anim;
 
-    // parameters
+    [Header("States")]
+    public bool isIdle;
+    public bool isWalking;
+    public bool isRunning;
+
     [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotationSmoothing;
@@ -30,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
     }
 
 
@@ -37,6 +44,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //TestInputAction();
         HandleMovement();
+        HandleAnimations();
 
     }
 
@@ -58,18 +66,31 @@ public class PlayerMovement : MonoBehaviour
         zMoveAmount = _movementVector.y;
         moveDirection = new Vector3(xMoveAmount, 0, zMoveAmount);
 
+        if (moveDirection == Vector3.zero)
+        {
+            isIdle = true;
+            isWalking = false;
+            isRunning = false;
+        }
+
         // execute move
         if (movementInput.IsPressed())
         {
+            isIdle = false;
+
             // apply walking logic
             float _walkMultiplier = Mathf.Clamp01(Mathf.Abs(xMoveAmount) + Mathf.Abs(zMoveAmount));
             if (_walkMultiplier > 0 && _walkMultiplier <= 0.9f)
             {
                 _walkMultiplier = 0.5f;
+                isWalking = true;
+                isRunning = false;
             }
             else if (_walkMultiplier > 0.9f && _walkMultiplier <= 1)
             {
                 _walkMultiplier = 1;
+                isWalking = false;
+                isRunning = true;
             }
 
             // rotate
@@ -82,6 +103,14 @@ public class PlayerMovement : MonoBehaviour
         }
 
 
+    }
+
+
+    private void HandleAnimations()
+    {
+        anim.SetBool("isIdle", isIdle);
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isRunning", isRunning);
     }
 
 }
